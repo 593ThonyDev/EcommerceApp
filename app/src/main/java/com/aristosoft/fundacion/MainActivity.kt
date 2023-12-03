4b09629903f82541c2d6e9a45ba91334de4a8e85
593ThonyDev <thonydev0726@gmail.com>
2023-12-03 11:26:25 -0500

    Initial commit

---
+++ app/src/main/java/com/aristosoft/fundacion/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.aristosoft.fundacion
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aristosoft.fundacion.ui.theme.FundacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.loadUrl("https://tallerint.netlify.app/#/")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FundacionTheme {
+        Greeting("Android")
+    }
+}
